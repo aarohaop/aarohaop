cd6f7859325c6d39c986d535e1fca826fd2b6be3
aarohaop <152183446+aarohaop@users.noreply.github.com>
2023-11-30 13:05:40 +0545

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ load_dotenv()
 API_ID = int(os.environ.get("API_ID", '21159773')) #API ID from https://my.telegram.org/auth
 API_HASH = os.environ.get("API_HASH", '49ae08543a07335e195756eba2f56e11') #API Hash from https://my.telegram.org/auth
 BOT_TOKEN = os.environ.get("BOT_TOKEN", '6538568462:AAEKlmAr7br3KRtmUVCD6sH6jWsArokT5k8') # Bot token from @BotFather
-ADMINS = [int(i.strip()) for i in os.environ.get("5886772061").split(",")] if os.environ.get("5886772061") else []
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("5886772061") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", 'SHARE')
 DATABASE_URL = os.environ.get("DATABASE_URL", 'mongodb+srv://SK:SK@cluster0.hbkrl.mongodb.net/?retryWrites=true&w=majority') # mongodb uri from https://www.mongodb.com/
