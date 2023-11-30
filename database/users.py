0dcbce55f72defa09186de20e4a08ae3a305000b
aarohaop <152183446+aarohaop@users.noreply.github.com>
2023-11-30 14:11:45 +0545

    Update users.py

--- database/users.py
+++ database/users.py
@@ -22,7 +22,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "ez4short.xyz",
+            "base_site": [SHORTNER_LINK],
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
